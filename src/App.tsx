d96be339cc9506326fa9ac88d79d45fa6294a34c
Sam Liu <samgliu19@gmail.com>
2022-05-01 22:52:41 -0400

    remove: removing unused type in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import MyTabs from './MyTabs';
 import React from 'react';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
 function App() {
   return (
     <div className="App">
